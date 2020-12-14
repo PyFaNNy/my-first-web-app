9a54cba74be93f5c84cb03cda073904c951c7396
Artyom <fanny.koob@gmail.com>
2020-12-14 15:26:44 +0300

    begin

---
+++ WebApplication1/WebApplication1/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class User :IdentityUser
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public string status { get; set; }
+        public DateTime dateReg { get; set; }
+        public DateTime dateLog { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace WebApplication1.Models
+{
+    public class UserContext : IdentityDbContext<User>
+    {
+        public DbSet<User> users { get; set; }
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
